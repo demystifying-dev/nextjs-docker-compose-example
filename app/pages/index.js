bf189ffcdbcd0210bef78a74124a1c26a3cb5dd1
Victor Kane <victorkane@gmail.com>
2020-04-12 17:11:57 -0300

    feat(theme): Manually incorporated Next.js official example with-rbx-bulma-pro, chosen because it is a natural continuation from bootstrap and material approaches but allows for SSR without any complications

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import Link from 'next/link'
+import { Section, Card, Content, Title } from 'rbx'
+import Layout from '../components/Layout'
 
-export default () => {
-  return (
-    <div>
-      <h1>Hello World, together we can do it!</h1>
-      <h2>Welcome to the world of hot loading dockerized Next.js!</h2>
-    </div>
-  )
-}
+const Home = () => (
+  <Layout>
+    <Section>
+      <Card>
+        <Card.Content>
+          <Content>
+            <Link href="https://github.com/zeit/next.js#getting-started">
+              <a>
+                <Title as="h3">Getting Started &rarr;</Title>
+                <p>Learn more about Next on Github and in their examples</p>
+              </a>
+            </Link>
+          </Content>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Content>
+          <Content>
+            <Link href="https://github.com/zeit/next.js/tree/master/examples">
+              <a>
+                <Title as="h3">Examples &rarr;</Title>
+                <p>
+                  Find other example boilerplates on the{' '}
+                  <code>create-next-app</code> site
+                </p>
+              </a>
+            </Link>
+          </Content>
+        </Card.Content>
+      </Card>
+      <Card>
+        <Card.Content>
+          <Content>
+            <Link href="https://github.com/zeit/next.js/tree/canary/packages/create-next-app">
+              <a>
+                <Title as="h3">Create Next App &rarr;</Title>
+                <p>Was this tool helpful? Let us know how we can improve it</p>
+              </a>
+            </Link>
+          </Content>
+        </Card.Content>
+      </Card>
+    </Section>
+  </Layout>
+)
+
+export default Home
---
+++ app/pages/more.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1>Hello World, together we can do it!</h1>
+      <h2>Welcome to the world of hot loading dockerized Next.js!</h2>
+    </div>
+  )
+}
